a60a106a0b669e6d9d8972f7033f7de7a8c2affa
viskin <r.viskin@terikon.com>
2017-02-25 16:10:25 +0200

    working code without megalist

--- www/js/index.js
+++ www/js/index.js
@@ -17,25 +17,67 @@ var app = {
     this.loadPhotos();
   },
 
-  loadPhotos: function() {
+  requestAuthorization: function () {
+
+    var self = this;
+
+    cordova.plugins.photoLibrary.requestAuthorization(
+      function () {
+        // Retry
+        self.loadPhotos();
+      },
+      function (err) {
+        console.log('Error in requestAuthorization: ' + err);
+
+        // TODO: explain to user why you need the permission, and continue when he agrees
+
+        // Ask user again
+        self.requestAuthorization();
+
+      }, {
+        read: true,
+        write: false
+      }
+    );
+
+  },
+
+  loadPhotos: function () {
+
+    var self = this;
 
     var gallery = $('#gallery');
 
     cordova.plugins.photoLibrary.getLibrary(
-      function (result) {
-        var library = result.library;
+      function (chunk) {
+        var library = chunk.library;
         // Here we have the library as array
 
         library.forEach(function (libraryItem) {
 
-          var image = $('<img>', { width: '100%', src: libraryItem.thumbnailURL, style: 'margin: 5px' });
+          var image = $('<img>', {
+            src: libraryItem.thumbnailURL,
+            style: 'margin: 5px; width: 100%;'
+          });
           image.appendTo(gallery);
 
         });
 
       },
       function (err) {
-        console.log('Error occured');
+        if (err.startsWith('Permission')) {
+
+          console.log('Please provide the permission');
+
+          // TODO: explain to user why you need the permission, and continue when he agrees
+
+          self.requestAuthorization();
+
+        } else { // Real error
+          console.log('Error in getLibrary: ' + err);
+        }
+      }, {
+        chunkTimeSec: 0.1,
       }
     );
 
